b601d9d6bef4d5dbf0e5757646c2b63be0105579
rekha218-alt <rockingrekha218@gmail.com>
2026-08-12 23:01:45 +0530

    exp-8.c

---
+++ C PROGRAM/exp-8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Grammar:\n");
+    printf("S -> AaAb | BbBa\n");
+    printf("A -> epsilon\n");
+    printf("B -> epsilon\n\n");
+
+    printf("FOLLOW(S) = { $ }\n");
+    printf("FOLLOW(A) = { a, b }\n");
+    printf("FOLLOW(B) = { b, a }\n");
+
+    return 0;
+}
